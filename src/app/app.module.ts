afa543b400b960031aed869138a64adf39e3a77b
Carlos Diaz <carlosdiaz.dev@gmail.com>
2023-10-22 21:16:48 -0600

    feat: implement lottie and countdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
 
 import { AppComponent } from './app.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+
+export function playerFactory(): any {
+  return import('lottie-web');
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountdownTimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/countdown-timer/countdown-timer.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+
+@Component({
+  selector: 'app-countdown-timer',
+  templateUrl: './countdown-timer.component.html',
+  styleUrls: ['./countdown-timer.component.scss']
+})
+export class CountdownTimerComponent implements OnInit{
+
+  options: AnimationOptions = {
+    path: '/assets/travel-tickets.json'
+  };
+
+
+  targetDate = moment('2023-12-16T23:59:59');
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+
+  ngOnInit() {
+    this.updateCountdown();
+    setInterval(() => this.updateCountdown(), 1000); // Actualizar cada segundo
+  }
+
+  onAnimate(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+
+
+  updateCountdown() {
+    const now = moment();
+    const duration = moment.duration(this.targetDate.diff(now));
+
+    this.days = duration.asDays() > 0 ? Math.floor(duration.asDays()) : 0;
+    this.hours = duration.hours() > 0 ? duration.hours() : 0;
+    this.minutes = duration.minutes() > 0 ? duration.minutes() : 0;
+    this.seconds = duration.seconds() > 0 ? duration.seconds() : 0;
+  }
+}
